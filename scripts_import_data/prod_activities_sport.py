7e7802f217c842e5068845976fdcf2f721e8b440
Tomtou1 <leroythomas01@yahoo.fr>
2026-02-11 14:47:31 +0100

    Creation_3_tables_SQL_Extract

--- scripts_import_data/prod_activities_sport.py
+++ scripts_import_data/prod_activities_sport.py
@@ -22,7 +22,7 @@ def connexion_sql():
     return conn
 
 def create_sql_table_sport_enterprise(conn):
-    # Create the table
+    # Create the table sport_enterprise
     cur = conn.cursor()
     cur.execute('''CREATE TABLE IF NOT EXISTS sport_enterprise (
     id_duo INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -50,6 +50,44 @@ def create_sql_table_sport_enterprise(conn):
             inserted_count += 1 
         conn.commit()
     print(f"{inserted_count} activités sportives insérées")
+
+
+def create_sql_table_RH(conn):
+    # Create the table RH_info
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS RH_info (
+    id_salarie INT NOT NULL PRIMARY KEY,
+    bu_salarie VARCHAR(50) NOT NULL,
+    salaire_brut INT NOT NULL,
+    type_contrat VARCHAR(10) NOT NULL,
+    jours_cp INT NOT NULL,
+    adresse_domicile TEXT NOT NULL, 
+    moyen_de_deplacement VARCHAR(50) NOT NULL
+    );''')
+    conn.commit()
+    
+    # Read the Excel file
+    excel_file_path = 'data/Donnees_RH.xlsx'
+    df = pd.read_excel(excel_file_path)
+    
+    # Insert data into the table
+    inserted_count = 0
+    for row in df.iterrows():
+        id_salarie = row[1]['ID salarié']
+        bu_salarie = row[1]['BU']
+        salaire_brut = row[1]['Salaire brut']
+        type_contrat = row[1]['Type de contrat']
+        jours_cp = row[1]['Nombre de jours de CP']
+        adresse_domicile = row[1]['Adresse du domicile']
+        moyen_de_deplacement = row[1]['Moyen de déplacement']
+
+        cur.execute("INSERT INTO RH_info (id_salarie, bu_salarie, salaire_brut, type_contrat, jours_cp, adresse_domicile, moyen_de_deplacement) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                (int(id_salarie), bu_salarie, salaire_brut, type_contrat, jours_cp, adresse_domicile, moyen_de_deplacement)
+            )
+        inserted_count += 1
+        conn.commit()
+    print(f"{inserted_count} informations RH insérées")
+
 
 def generate_history_activity(conn):
     range_start_date = datetime(2025, 1, 1).timestamp()
@@ -83,8 +121,9 @@ if __name__ == "__main__":
     conn = connexion_sql()
     cur = conn.cursor()
     
-    #Create table wih activities of each employee
+    #Create table wih activities of each employee, and RH table with info about employees
     create_sql_table_sport_enterprise(conn)
+    create_sql_table_RH(conn)
 
     #Create history of activity
     n_history_activities = 10
