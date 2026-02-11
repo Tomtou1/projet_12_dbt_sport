8dff49f4b0000055c5057245654edc2a5e0e3eb8
Tomtou1 <leroythomas01@yahoo.fr>
2026-02-11 14:33:59 +0100

    Create_table_history

---
+++ DB_SQL/strava.sql
@@ -0,0 +1,17 @@
+CREATE TABLE sport_activities (
+    id_activity INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id_salarie INT NOT NULL,
+    date_start TIMESTAMP NOT NULL,
+    type_activity VARCHAR(50) NOT NULL,
+    distance_m INT,
+    date_end TIMESTAMP NOT NULL,
+    comments TEXT
+);
+
+ALTER TABLE sport_activities
+REPLICA IDENTITY FULL;
+
+-- 1ère insertion de données pour la table sport_activities
+INSERT INTO sport_activities (id_salarie, date_start, type_activity, distance_m, date_end, comments)
+VALUES
+( 56482, '2025-01-02 10:00:00', 'Tennis', NULL, '2025-01-02 11:00:00', 'Super match avec mon frère!');
